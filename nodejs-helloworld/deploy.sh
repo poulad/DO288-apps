be8228c928dcd6ea423b15c5855900c53077edb2
Poulad <poulad.pld@gmail.com>
2020-09-11 15:52:48 -0400

    lab exercise

---
+++ nodejs-helloworld/deploy.sh
@@ -0,0 +1,7 @@
+source .env.tmp
+
+oc delete project --all
+
+oc new-project ${RHT_OCP4_DEV_USER}-source-build
+
+oc new-app --as-deployment-config --name greet --build-env npm_config_registry="http://${RHT_OCP4_NEXUS_SERVER}/repository/nodejs" nodejs:12~https://github.com/${RHT_OCP4_GITHUB_USER}/DO288-apps#source-build --context-dir nodejs-helloworld
